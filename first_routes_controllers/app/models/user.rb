b6960a7b329b483b59f9570cd00eda62471b9ed1
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-02-16 11:48:28 -0500

    Setting up Artwork Shares

--- first_routes_controllers/app/models/user.rb
+++ first_routes_controllers/app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    validates :name, :email, presence: true
+    validates :username, presence: true, uniqueness: true
+
+    has_many :artworks,
+        primary_key: :id,
+        foreign_key: :artist_id,
+        inverse_of: :artist,
+        class_name: :Artwork,
+        dependent: :destroy
 end
---
+++ first_routes_controllers/db/migrate/20230216163727_create_artwork_share.rb
@@ -0,0 +1,12 @@
+class CreateArtworkShare < ActiveRecord::Migration[7.0]
+  def change
+    create_table :artwork_shares do |t|
+      t.references :artwork, foreign_key: true, null: false
+      t.references :viewer, foreign_key: {to_table: :users}, null: false
+
+      t.timestamps
+    end
+
+    add_index :artwork_shares, [:viewer_id, :artwork_id], unique: true
+  end
+end
